ade33c2f402dfcdaca45ebc1d56ddbf199f48683
Dongzheng Wu <dwuu0018@student.monash.edu>
2025-09-21 14:21:28 +1000

    Improved structure by adding a layer between controller and database repository

--- src/main/java/au/com/telstra/simcardactivator/component/SimCardActivateController.java
+++ src/main/java/au/com/telstra/simcardactivator/component/SimCardActivateController.java
@@ -1,39 +1,26 @@
 package au.com.telstra.simcardactivator.component;
 
-import au.com.telstra.simcardactivator.database.SimInfo;
-import au.com.telstra.simcardactivator.database.SimInfoRepository;
-import au.com.telstra.simcardactivator.database.SimInfoResponse;
 import au.com.telstra.simcardactivator.foundation.SimCard;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SimCardActivateController {
     private final SimCardActuationHandler simCardActuationHandler;
-    private final SimInfoRepository simInfoRepository;
+    private final DatabaseConduit databaseConduit;
 
-    public SimCardActivateController(SimCardActuationHandler simCardActuationHandler, SimInfoRepository simInfoRepository) {
+    public SimCardActivateController(SimCardActuationHandler simCardActuationHandler, DatabaseConduit databaseConduit) {
         this.simCardActuationHandler = simCardActuationHandler;
-        this.simInfoRepository = simInfoRepository;
+        this.databaseConduit = databaseConduit;
     }
 
     @PostMapping(value = "/activate")
     public void handleActivationRequest(@RequestBody SimCard simCard) {
         var actuationResult = simCardActuationHandler.actuate(simCard);
-
-        SimInfo record = new SimInfo(simCard.getIccid(), simCard.getCustomerEmail(), actuationResult.getSuccess());
-        simInfoRepository.save(record);
-
-        System.out.println(actuationResult);
+        databaseConduit.save(simCard, actuationResult);
     }
 
     @GetMapping(value = "/search")
-    public ResponseEntity<SimInfoResponse> handleSearchRequest(@RequestParam Long simCardId) {
-        return simInfoRepository.findById(simCardId)
-                .map(record -> new ResponseEntity<>(
-                        new SimInfoResponse(record.getIccid(), record.getCustomerEmail(), record.isActive()),
-                        HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public SimCard handleSearchRequest(@RequestParam Long simCardId) {
+        return databaseConduit.querySimCard(simCardId);
     }
 }
--- src/main/java/au/com/telstra/simcardactivator/database/SimInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package au.com.telstra.simcardactivator.database;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface SimInfoRepository extends CrudRepository<SimInfo, Long> { }
